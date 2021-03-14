7b89bf17d1ee767a5bd7b65ceadb8ece033a0e6a
Tom Casavant <casavant.6@buckeyemail.osu.edu>
2021-03-14 14:10:51 -0400

    Redesigned create group layout with custom dialog and fixed up code to remove warnings regarding Java 8+ compatability. Also, fixed warnings across project

--- app/src/main/java/com/group9/grouptivity/SecondFragment.java
+++ app/src/main/java/com/group9/grouptivity/SecondFragment.java
@@ -23,12 +23,9 @@ public class SecondFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.activities_fragment_groups_button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        view.findViewById(R.id.activities_fragment_groups_button).setOnClickListener( (View v) ->
                 NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
-            }
-        });
+                        .navigate(R.id.action_SecondFragment_to_FirstFragment)
+        );
     }
 }
