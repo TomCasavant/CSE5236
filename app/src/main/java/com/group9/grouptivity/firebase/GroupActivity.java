ed08be5c3d57400e6a22a997e78e0b2c0cb9b5ed
NKSpann <derin161@gmail.com>
2021-03-14 21:40:13 -0400

    Added message and GroupActivity model classes.

---
+++ app/src/main/java/com/group9/grouptivity/firebase/GroupActivity.java
@@ -0,0 +1,28 @@
+package com.group9.grouptivity.firebase;
+
+public class GroupActivity {
+    private String mActivityType; //Maybe use an enum?
+    private double mExpectedCost;
+    private String mLocation;
+
+    public GroupActivity(String activityType, double expectedCost, String location) {
+        this.mActivityType = activityType;
+        this.mExpectedCost = expectedCost;
+        this.mLocation = location;
+    }
+
+    /** Returns the type of activity that this is. */
+    private String getActivityType() {
+        return this.mActivityType;
+    }
+
+    /** Returns the expected cost associated with this activity. */
+    private double getExpectedCost() {
+        return  this.mExpectedCost;
+    }
+
+    /** Returns the location associated with this activity. */
+    private String getLocation() {
+        return this.mLocation;
+    }
+}
